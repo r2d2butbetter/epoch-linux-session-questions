1dd2b1a836ee7b4e7c4476fd24ee7c31c8d69e36
r2d2butbetter <150259040+r2d2butbetter@users.noreply.github.com>
2024-08-25 16:37:57 +0530

    Added files for challenge4

---
+++ challenge4/challenge04.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define WANTED_PATH "/home/challenge4/creating/files/and/directories/is/easy"
+// HEh IM SO EVIL
+// The flag is not even part of the program :0
+int main(void)
+{
+	printf("What flag? I don't see no flag!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Flag EPOCH{Dr_Evil_is_dumb?}
