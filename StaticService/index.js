4356dd74856f32340768c7721942ed4ea7b5a9f8
r2d2butbetter <150259040+r2d2butbetter@users.noreply.github.com>
2024-08-26 21:38:24 +0530

    Add files via upload
    Added the files required for the static file service. We ended up just serving a static index.html file.

---
+++ StaticService/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+//import fs from "fs";
+//import path from "path";
+
+const app=express();
+app.use(express.static("public"));
+//const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res)=>{
+	res.sendFile("index.html");
+});
+app.listen(5000, ()=>{
+	console.log("shriansh is gay and server is running");
+})
